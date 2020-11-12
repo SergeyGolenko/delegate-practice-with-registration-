8c95d3bc95b7ba1f2a63bd527b46f92cc898ac4d
SergeyGolenko <1989sergiotay@gmail.com>
2020-11-12 16:49:37 +0200

    Initial Commit

---
+++ Delegate(передача данных с RegistrationViewController)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Delegate(передача данных с RegistrationViewController)
+//
+//  Created by Сергей Голенко on 12.11.2020.
+//  Copyright © 2020 Сергей Голенко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
